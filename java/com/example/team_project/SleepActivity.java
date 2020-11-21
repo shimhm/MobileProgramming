c9d4e05ca94cac37f87b3f08a82a03f5cc4f043d
shimhm <55525917+shimhm@users.noreply.github.com>
2020-11-21 13:47:54 +0900

    Add files via upload
    main menu 화면과 menu 내부 화면

--- java/com/example/team_project/SleepActivity.java
+++ java/com/example/team_project/SleepActivity.java
@@ -17,10 +17,27 @@ public class SleepActivity extends Activity {
 
         ImageView btn_back = (ImageView) findViewById(R.id.btn_back);
 
+        final ImageView img_gyro = (ImageView) findViewById(R.id.sleep_gyro);
+        final ImageView img_CDs = (ImageView) findViewById(R.id.sleep_CDs);
+
+        final Button btn_sleep_start = (Button) findViewById(R.id.sleep_start);
+
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
+            }
+        });
+
+        //SleepActivity 열 때 MenuActivity에서 특정 값을 넘겨줘서 수면중인지 판단해야함.
+        btn_sleep_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(btn_sleep_start.getText().toString().equals("수면시작")){
+                    btn_sleep_start.setText("수면종료");
+                } else {
+                    btn_sleep_start.setText("수면시작");
+                }
             }
         });
     }
--- java/com/example/team_project/StudyActivity.java
+++ java/com/example/team_project/StudyActivity.java
@@ -1,6 +1,7 @@
 package com.example.team_project;
 
 import android.app.Activity;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -10,6 +11,7 @@ import androidx.annotation.Nullable;
 
 public class StudyActivity extends Activity {
 
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,11 +19,37 @@ public class StudyActivity extends Activity {
 
         ImageView btn_back = (ImageView) findViewById(R.id.btn_back);
 
+        final ImageView img_gyro = (ImageView) findViewById(R.id.study_gyro);
+        final ImageView img_CDs = (ImageView) findViewById(R.id.study_CDs);
+        final ImageView img_dist = (ImageView) findViewById(R.id.study_dist);
+
+        final Button btn_study_start = (Button) findViewById(R.id.study_start);
+
         btn_back.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 finish();
             }
         });
+
+        img_gyro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                img_gyro.setImageResource(R.drawable.good);
+            }
+        });
+
+        //StudyActivity 열 때 MenuActivity에서 특정 값을 넘겨줘서 공부중인지 판단해야함.
+        btn_study_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(btn_study_start.getText().toString().equals("공부시작")){
+                    btn_study_start.setText("공부종료");
+                } else {
+                    btn_study_start.setText("공부시작");
+                }
+            }
+        });
+
     }
 }
